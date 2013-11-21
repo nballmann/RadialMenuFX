b73761d02507581f3047fcc37d035b6ce0561207
nballmann <Administrator@10.0.2.15>
2013-11-21 16:53:59 +0100

    added: -math. correct way of adding symbols to ring-segments -popup-animation -early javadoc changed: -positioning of RadialMenu -construction of instance -closing behavior

--- RadialMenuFx/src/org/nic/menu/MainApp.java
+++ RadialMenuFx/src/org/nic/menu/MainApp.java
@@ -1,10 +1,12 @@
 package org.nic.menu;
 
+import javafx.animation.ScaleTransition;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -12,16 +14,23 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Path;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
+import javafx.util.Duration;
 
 public class MainApp extends Application {
 
+    private static final int SEGMENT_COUNT = 6;
+    
     private boolean pressed = false;
 
     private Stage primaryStage;
-    
+    private Stage menuStage;
+
+    private ScaleTransition scaleIn;
+
     @Override
     public void start(Stage primaryStage) {
-	
+
 	this.primaryStage = primaryStage;
 
 	AnchorPane pane = new AnchorPane();
@@ -38,6 +47,7 @@ public class MainApp extends Application {
 		    public void run()
 		    {
 			pressed = true;
+			
 			while(pressed)
 			{
 			    try
@@ -52,7 +62,9 @@ public class MainApp extends Application {
 				    @Override
 				    public void run()
 				    {
-					initRadialMenu(event.getScreenX()-250, event.getScreenY()-250);
+					showMenu(event.getScreenX()-(menuStage.getWidth()/2), event.getScreenY()-(menuStage.getHeight()/2));
+
+					System.out.println(menuStage.getWidth()/2 + " : " + menuStage.getHeight()/2);
 					pressed = false;
 				    }
 
@@ -76,6 +88,8 @@ public class MainApp extends Application {
 
 	});
 
+	initRadialMenu();
+
 	Scene scene = new Scene(pane);
 	scene.setFill(null);
 
@@ -84,25 +98,25 @@ public class MainApp extends Application {
 	primaryStage.setResizable(false);
 	primaryStage.show();
 
-
-
-	//	    pane.getChildren().addAll(RadialMenu.outerCircleSegment(
-	//		    250, 250, 200, 100, 12, 
-	//		    Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255)), 
-	//		    Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255))));
+	scaleIn = new ScaleTransition(Duration.millis(300));
+	scaleIn.setFromX(0);
+	scaleIn.setFromY(0);
+	scaleIn.setToX(1.0);
+	scaleIn.setToY(1.0);
     }
 
-
-    private void initRadialMenu(double posX, double posY)
+    private void initRadialMenu()
     {
 	final Stage stage = new Stage(StageStyle.TRANSPARENT);
 	stage.setResizable(false);
+	stage.setWidth(300);
+	stage.setHeight(300);
+	//	stage.initModality(Modality.APPLICATION_MODAL);
 	stage.initOwner(primaryStage);
-	stage.setX(posX);
-	stage.setY(posY);
-	
-	final RadialMenu menu = new RadialMenu(250, 250, 200, 100, 6, Color.RED, Color.BLACK);
-	
+
+	final RadialMenu menu = new RadialMenu(150, 150, 150, 80, SEGMENT_COUNT, Color.RED, Color.BLACK);
+
+	menu.setRotate(360/SEGMENT_COUNT*(SEGMENT_COUNT*0.57));
 	menu.focusTraversableProperty().set(true);
 	menu.focusedProperty().addListener(new ChangeListener<Boolean>() {
 
@@ -119,59 +133,65 @@ public class MainApp extends Application {
 			{
 			    stage.close();
 			}
-			
+
 		    });
 		}
 	    }
-	    
+
 	});
 
-	for(final Path p : menu.getSegments())
+	for(final Group g : menu.getSegments())
 	{
-	    p.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+	    g.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
 
 		@Override
 		public void handle(MouseEvent e)
 		{
-		    p.setFill(Color.WHITE);
+		    if(g.equals(menu.getSegments().get(0)))
+		    {
+			((Path) g.getChildren().get(0)).setFill(Color.BLUE);
+		    }
+		    else
+			((Path) g.getChildren().get(0)).setFill(Color.WHITE);
 		}
 
 	    });
 
-	    p.addEventHandler(MouseEvent.MOUSE_EXITED, new EventHandler<MouseEvent>() {
+	    g.addEventHandler(MouseEvent.MOUSE_EXITED, new EventHandler<MouseEvent>() {
 
 		@Override
 		public void handle(MouseEvent e)
 		{
-		    p.setFill(Color.BLACK);
+		    ((Path) g.getChildren().get(0)).setFill(Color.BLACK);
 		}
 
 	    });
 	}
 
-
-	Scene scene = new Scene(menu);
+	final Scene scene = new Scene(menu);
 	scene.setFill(null);
-	
-	stage.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+
+	stage.setOnShowing(new EventHandler<WindowEvent>() {
 
 	    @Override
-	    public void handle(MouseEvent e)
+	    public void handle(WindowEvent e)
 	    {
-		if( e.getSource() == menu )
-		{
-		    stage.close();
-		}
+		scaleIn.setNode(menu);
+		scaleIn.play();		
 	    }
 
 	});
 
-
 	stage.setScene(scene);
-	stage.show();
+	menuStage = stage;
     }
 
-
+    private void showMenu(double posX, double posY)
+    {
+	menuStage.setX(posX);
+	menuStage.setY(posY);
+	menuStage.show();
+    }
 
     public static void main(String[] args) {
 	launch(args);
--- RadialMenuFx/src/org/nic/menu/RadialMenu.java
+++ RadialMenuFx/src/org/nic/menu/RadialMenu.java
@@ -2,6 +2,7 @@ package org.nic.menu;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Group;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcTo;
@@ -9,31 +10,53 @@ import javafx.scene.shape.FillRule;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
+import javafx.scene.shape.Rectangle;
 
+/**
+ * Extension PopUp-Menu for JavaFX 2.x
+ * 
+ * 
+ * @author N.Ballmann
+ *
+ */
 public class RadialMenu extends Region
 {
     private double centerX, centerY, outerRadius, innerRadius;
     private int segmentCount;
     private Color strokeColor, fillColor;
     
-    private ObservableList<Path> segments = FXCollections.observableArrayList();
+    private ObservableList<Group> segments = FXCollections.observableArrayList();
     
     /**
+     * getter for the ObservableList representing this RadialMenus ring-segment-nodes
      * @return the segments
      */
-    public ObservableList<Path> getSegments()
+    public ObservableList<Group> getSegments()
     {
         return segments;
     }
 
     /**
-     * @param segments the segments to set
+     * setter for the ObservableList representing this RadialMenus ring-segment-nodes
+     * @param segments the ObservableList to set
      */
-    public void setSegments(ObservableList<Path> segments)
+    public void setSegments(ObservableList<Group> segments)
     {
         this.segments = segments;
     }
 
+    /**
+     * Creates a new RadialMenu Node<br>
+     * Listeners can be registered onto the Group-Nodes inside of the ObservableList<br>
+     * retrievable by its getter {@link getSegments}
+     * @param centerX the horizontal center of the node inside its {@link Scene} 
+     * @param centerY the vertical center of the node inside its {@link Scene} 
+     * @param outerRadius the outer radius of the {@linkRadialMenu}
+     * @param innerRadius the inner radius of the {@linkRadialMenu}
+     * @param segmentCount number of segments/click-able parts inside the RadialMenu
+     * @param strokeColor the outside stroke color of the RadialMenu
+     * @param fillColor the fill color of the RadialMenu
+     */
     public RadialMenu(double centerX, double centerY, double outerRadius, double innerRadius, int segmentCount, Color strokeColor, Color fillColor)
     {
 	this.centerX = centerX;
@@ -51,12 +74,22 @@ public class RadialMenu extends Region
 	this.getChildren().addAll(segments);
     }
 
+    /**
+     * computes the circular ring segments specified by segmentCount 
+     * through translation of vector coordinates into local coordinate-space at
+     * application runtime <br>
+     * Each segment and its associated symbol will be added to a 
+     * group-node which will be put into an ObservableList of type Group: segments <br>
+     * @see {@link segments}, {@link getSegments}, {@link setSegments}
+     */
     private void createOuterCircleSegments()
     {
 	double segmentRadians = 360/segmentCount*Math.PI/180;
 
 	for(int i = 2; i <= segmentCount+1; i++)
 	{
+	    Group g = new Group();
+	    
 	    Path p = new Path();
 	    p.setFill(fillColor);
 	    p.setStroke(strokeColor);
@@ -102,7 +135,21 @@ public class RadialMenu extends Region
 	    p.getElements().add(innerArc);
 	    p.getElements().add(end);
 	    
-	    segments.add(p);
+	    g.getChildren().add(p);
+	    
+	    
+	    // temporary: for button layout injection
+	    Rectangle rect = new Rectangle();
+	    rect.setX(centerX-((outerRadius-innerRadius)*0.35) + (innerRadius + (outerRadius - innerRadius)/2)*Math.cos(segmentRadians*(i-.5)));
+	    rect.setY(centerY-((outerRadius-innerRadius)*0.35) + (innerRadius + (outerRadius - innerRadius)/2)*Math.sin(segmentRadians*(i-.5)));
+	    rect.setWidth(((outerRadius-innerRadius)*0.7));
+	    rect.setHeight(((outerRadius-innerRadius)*0.7));
+	    rect.setRotate(Math.toDegrees(segmentRadians*(i-.5)));
+	    rect.setFill(Color.RED);
+	    
+	    g.getChildren().add(rect);
+	    
+	    segments.add(g);
 	}
 
     }
